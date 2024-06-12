53faf149d14e874c1f03168f01207ed3147180f6
ShiXun0604 <tomcruise890604@gmail.com>
2024-06-12 18:19:52 +0800

    2024/6/12

---
+++ dynamic-programming/264_Ugly Number II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        index_2, index_3, index_5 = 0, 0, 0
+        ans = [1]
+
+        for i in range(n):
+            num = min(ans[index_2]*2, ans[index_3]*3, ans[index_5]*5)
+            ans.append(num)
+
+            if num == ans[index_2]*2:
+                index_2 += 1
+            if num == ans[index_3]*3:
+                index_3 += 1
+            if num == ans[index_5]*5:
+                index_5 += 1
+        
+        print(ans)
+        return ans[n-1]
+            
+
+
+n = 11
+ans = Solution().nthUglyNumber(n)
+print(ans)
--- temp.py
+++ temp.py
@@ -1,29 +1,38 @@
-class TreeNode():
-    def __init__(self, val=0, left=None, right=None, next=None):
-        self.val = val
-        self.left = left
-        self.right = right
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
+def send_email(sender_email, app_password, recipient_email, subject, body):
+    # 設定 SMTP 伺服器及其埠號
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
 
-class Solution():
-    def __init__(self) -> None:
-        self.ans = []
-    
-    def __recur_gen(self, result, left, right) -> None:
-        pass
-        if left == 0:
-            self.ans.append(result + ")"*right)
-            return
-        
-        self.__recur_gen(result+'(', left-1, right)
-        if left < right:
-            self.__recur_gen(result+')', left, right-1)
-        
-    
-    def generateParenthesis(self, n: int) -> list[str]:
-        self.__recur_gen('', n, n)
-        return self.ans
+    # 建立 MIMEMultipart 物件
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = subject
 
+    # 添加郵件正文內容
+    message.attach(MIMEText(body, "plain"))
 
-s = 'abab'
-print(s[0:3])
+    try:
+        # 建立與 Gmail SMTP 伺服器的連接
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls()  # 升級連接到加密的 TLS
+        server.login(sender_email, app_password)  # 登入 Gmail 帳戶
+        server.sendmail(sender_email, recipient_email, message.as_string())  # 發送郵件
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server.quit()  # 關閉連接
+
+# 使用範例
+sender_email = "tomcruise890604@gmail.com"
+app_password = "hrmm egho mggo oarx"
+recipient_email = "shixun.chen@expertos.com.tw"
+subject = "Test Email"
+body = "This is a test email sent from Python."
+
+send_email(sender_email, app_password, recipient_email, subject, body)
